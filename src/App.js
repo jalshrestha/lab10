5fddf5312cec709a414e883d440890273b69e3c6
JAL Shrestha <jaljalalama56@gmail.com>
2023-03-25 20:54:23 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Container from './Container';
+import MyList from './ItemList';
+const items = [
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+];
+const myStyle = {
+  backgroundColor: 'blue',
+  color: 'white',
+  padding: '10px 20px',
+  border: 'none',
+  borderRadius: '5px',
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+       <h1>This is react app!</h1>
+       <Container style ={myStyle} />
+       <h1  style={{ fontWeight: 'italic' }}>My List</h1>
+       <MyList items={items} className ="list"/>
       </header>
+     
     </div>
   );
 }
